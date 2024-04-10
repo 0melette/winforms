c3a9afacb5e9c2cb82757b7b0244a384c7d96260
Amy Zhou <amy.zhou@wisetechglobal.com>
2024-04-10 11:45:38 +1000

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WindowsFormsApp1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.panel2 = new System.Windows.Forms.Panel();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+			this.panel1.Location = new System.Drawing.Point(130, 168);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(423, 212);
+			this.panel1.TabIndex = 0;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Checked = true;
+			this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBox1.Location = new System.Drawing.Point(1389, 1085);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(239, 36);
+			this.checkBox1.TabIndex = 0;
+			this.checkBox1.Text = "i love winforms";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+			// 
+			// monthCalendar1
+			// 
+			this.monthCalendar1.Location = new System.Drawing.Point(612, 185);
+			this.monthCalendar1.Name = "monthCalendar1";
+			this.monthCalendar1.TabIndex = 1;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.Screenshot_2024_03_14_142713;
+			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.pictureBox1.Location = new System.Drawing.Point(206, 956);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(1035, 304);
+			this.pictureBox1.TabIndex = 2;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "the ",
+            "quick",
+            "cat",
+            "cat",
+            "dog"});
+			this.comboBox1.Location = new System.Drawing.Point(110, 406);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(463, 39);
+			this.comboBox1.TabIndex = 3;
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(498, 739);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(8, 90);
+			this.progressBar1.TabIndex = 4;
+			// 
+			// panel2
+			// 
+			this.panel2.Location = new System.Drawing.Point(1372, 719);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(8, 8);
+			this.panel2.TabIndex = 0;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(2433, 1621);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.monthCalendar1);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.MonthCalendar monthCalendar1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Panel panel2;
+	}
+}
+
